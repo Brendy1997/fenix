c677f5f49b3a18db1ce6916f081e53b6a3124e7e
Brendy1997 <btamayooca@gmail.com>
2022-09-03 19:07:57 -0400

    font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SidebarComponent
+    SidebarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
