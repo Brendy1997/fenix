72fcd2bfcb86d246e08588f1b2ec1db3d54759ab
Brendy1997 <btamayooca@gmail.com>
2022-09-05 13:01:53 -0400

    nav-bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  network = "BNB Smart Chain";
+  wallet = "Connect Wallet";
+  currentWallet = false;
+
+  networks = ["Etherium", "BNB Smart Chain", "Matic(Polygon)", "Avalanche", "Fantom Opera", "Cronos"];
+
+  wallets = ["Metamask", "TrustWallet", "WalletConnect", "Coinbase Wallet", "SafePal Wallet", "TokenPocket"];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  SelectNetwork(n: string) {
+    this.network = n;
+    this.closeModal();
+  }
+
+  SelectWallet(n: string) {
+    this.wallet = n;
+    this.currentWallet = true;
+    this.closeModal();
+  }
+
+  CloseWallet() {
+    this.wallet = "Connect Wallet";
+    this.currentWallet = false;
+  }
+
+  OpenModal(id: string) {
+    let select = document.getElementById(id);
+    if (select) {
+      select.classList.add("show");
+    }
+  }
+
+  closeModal() {
+    let modal = document.getElementsByClassName("modal2");
+    for (let m = 0; m < modal.length; m++) {
+      modal[m].classList.remove('show');
+    }
   }
 
 }
